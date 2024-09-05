0726e2d0e5249fbf09e7c05d4580fb72a1e0b094
jeongminDev <bjm4256@gmail.com>
2024-09-05 07:32:43 +0000

    [FIX] 빌드 수정

---
+++ components/Button.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+interface ButtonProps {
+  text: string;
+  onClick?: () => void;
+}
+
+const Button: React.FC<ButtonProps> = ({ text, onClick, ...props }) => {
+  return (
+    <button className="bg-blue-500 text-white py-2 px-4 rounded" {...props} onClick={onClick}>
+      {text}
+    </button>
+  );
+};
+
+export default Button;
